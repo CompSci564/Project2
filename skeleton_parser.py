b725908bb35840d6efda43c8eafe159ea52b66b3
youngwoo <ykim846@wisc.edu>
2024-02-16 01:57:33 -0600

    works locally

--- skeleton_parser.py
+++ skeleton_parser.py
@@ -31,7 +31,7 @@ from re import sub
 columnSeparator = "|"
 
 # Dictionary of months used for date transformation
-MONTHS = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',
+MONTHS = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',\
         'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
 
 """
@@ -68,148 +68,51 @@ def transformDollar(money):
         return money
     return sub(r'[^\d.]', '', money)
 
-"""
-Parses a single json file. Currently, there's a loop that iterates over each
-item in the data set. Your job is to extend this functionality to create all
-of the necessary SQL tables for your database.
-"""
 def parseJson(json_file):
     with open(json_file, 'r') as f:
         items = loads(f.read())['Items'] # creates a Python dictionary of Items for the supplied json file
-        file1 = open("category.dat", "a")
-        file2 = open("user.dat", "a")
-        file3 = open("bids.dat", 'a')
-        file4 = open("items.dat", 'a')
         for item in items:
-            """
-            TODO: traverse the items dictionary to extract information from the
-            given `json_file' and generate the necessary .dat files to generate
-            the SQL tables based on your relation design
-            """
-            # category table
-            categories = item["Category"]
-            itemID = item["ItemID"]
-            if itemID == None:
-                itemID = "NULL"
-            for category in categories:
-                line = ['\"' + sub('\"','\"\"',category) + '\"', itemID]
-                file1.write("|".join(line) + "\n")
-                
-            # items table
-            itemRow = []
-            if not item["ItemID"] == None:
-                itemRow.append(str(item["ItemID"]))
-            else:
-                itemRow.append("NULL")
-            if not item["Seller"]["UserID"] == None:
-                itemRow.append('\"' + sub('\"','\"\"',str(item["Seller"]["UserID"])) + '\"')
-            else:
-                itemRow.append("NULL")
-            if not item["Name"] == None:
-                itemRow.append('\"' + sub('\"','\"\"',str(item["Name"])) + '\"')
-            else:
-                itemRow.append("NULL")
-            if not item["Number_of_Bids"] == None:
-                itemRow.append(item["Number_of_Bids"])
-            else:
-                itemRow.append("NULL")
-            if not item["First_Bid"] == None:
-                itemRow.append(str(transformDollar(item["First_Bid"])))
-            else:
-                itemRow.append("NULL")
-            if not "Buy_Price" in item.keys() or item["Buy_Price"] == None:
-                itemRow.append("NULL")
-            else:
-                itemRow.append(str(transformDollar(item["Buy_Price"])))
-            if not item["Currently"] == None:
-                itemRow.append(str(transformDollar(item["Currently"])))
-            else:
-                itemRow.append("NULL")
-            if not item["Started"]:
-                itemRow.append(str(transformDttm(item["Started"])))
-            else:
-                itemRow.append("NULL")
-            if not item["Ends"] == None:
-                itemRow.append(str(transformDttm(item["Ends"])))
-            else:
-                itemRow.append("NULL")
-            if not item["Description"] == None:
-                itemRow.append('\"' + sub('\"','\"\"',str(item["Description"])) + '\"')
-            else:
-                itemRow.append("NULL")
-            strItemRow = "|".join(itemRow) + "\n"
-            file4.write(strItemRow)
-                               
-            # users table: sellers
-            if not item["Seller"]["UserID"] == None:
-                users = ['\"' + sub('\"','\"\"',item["Seller"]["UserID"]) + '\"']
-            else:
-                users = ["NULL"]
-            if not item["Location"] == None:
-                users.append('\"' + sub('\"','\"\"',item["Location"]) + '\"')
-            else:
-                users.append("NULL")
-            if not item["Country"] == None:
-                users.append('\"' + sub('\"','\"\"',item["Country"]) + '\"')
-            else:
-                users.append("NULL")
-            if not item["Seller"]["Rating"] == None:
-                users.append(item["Seller"]["Rating"])
-            else:
-                users.append("NULL")
-            file2.write("|".join(users) + "\n")
-            
-            # users table: bidders
-            if not item["Bids"] == None:
-                for i in range(len(item["Bids"])):
-                    bidder = item["Bids"][i]["Bid"]["Bidder"]
-                    if not bidder["UserID"] == None:
-                        line = ['\"' + sub('\"','\"\"',bidder["UserID"]) + '\"']
-                    else:
-                        line = ["NULL"]
-                    if not "Location" in bidder.keys() or bidder["Location"] == None:
-                        line.append("NULL")
-                    else:
-                        line.append('\"' + sub('\"','\"\"',bidder["Location"]) + '\"')
-                    if not "Country" in bidder.keys() or bidder["Country"] == None:
-                        line.append("NULL")
-                    else:
-                        line.append('\"' + sub('\"','\"\"',bidder["Country"]) + '\"')
-                    if not bidder["Rating"] == None:
-                        line.append(bidder["Rating"])
-                    else: 
-                        line.append("NULL")
-                    file2.write("|".join(line) + "\n")
-                              
-            # bids table
-            bids = item["Bids"]
-            if item["ItemID"] == None:
-                item_id = "NULL"
-            else:
-                item_id = item["ItemID"]
-            if bids == None:
-                # add the item id and have the rest of the attributes be null
-                file3.write(item_id + "|" + "NULL" + "|" + "NULL" + "|" + "NULL" + "\n")
-                continue
-            for bid in bids:
-                if bid["Bid"]["Bidder"]["UserID"] == None:
-                    bidder_id = "NULL"
-                else:
-                    bidder_id = '\"' + sub('\"','\"\"',bid["Bid"]["Bidder"]["UserID"]) + '\"'
-                if bid["Bid"]["Time"] == None:
-                    bid_time = "NULL"
-                else:
-                    bid_time = transformDttm(bid["Bid"]["Time"]) 
+            item_id = str(item['ItemID'])
+            name = item['Name'].replace('"', '""')
+            categories = item['Category']
+            currently = transformDollar(item['Currently'])
+            buy_price = transformDollar(item.get('Buy_Price', 'NULL')) if 'Buy_Price' in item else 'NULL'
+            first_bid = transformDollar(item['First_Bid'])
+            number_of_bids = str(item['Number_of_Bids'])
+            started = transformDttm(item['Started'])
+            ends = transformDttm(item['Ends'])
+            description = item['Description'] if 'Description' in item else 'NULL'
 
-                if bid["Bid"]["Amount"] == None:
-                    bid_amount = "NULL"
-                else:
-                    bid_amount = transformDollar(bid["Bid"]["Amount"])
-                file3.write(item_id + "|" + bidder_id + "|" + bid_time + "|" + bid_amount + "\n")
-        file1.close()
-        file2.close()
-        file3.close()
-        file4.close()
+            with open('categories.dat', 'a') as cat_file:
+                for category in categories:
+                    cat_file.write(f"{item_id}{columnSeparator}\"{category}\"\n")
+
+            seller_id = item['Seller']['UserID'].replace('"', '""')
+            seller_rating = str(item['Seller']['Rating'])
+            with open('users.dat', 'a') as user_file:
+                user_file.write(f"{seller_id}{columnSeparator}{seller_rating}\n")
+
+            with open('items.dat', 'a') as items_file:
+                items_file.write(f"{item_id}{columnSeparator}\"{name}\"{columnSeparator}\"{description}\"{columnSeparator}{currently}{columnSeparator}{buy_price}{columnSeparator}{first_bid}{columnSeparator}{number_of_bids}{columnSeparator}{started}{columnSeparator}{ends}\n")
+
+            bids = item.get('Bids', [])
+            if bids:
+                for bid_package in bids:  # Assuming each 'bid_package' contains the actual 'Bid' information
+                    bid = bid_package.get('Bid', {})
+                    bid_amount = transformDollar(bid.get('Amount', 'NULL'))
+                    bid_time = transformDttm(bid.get('Time', 'NULL'))
+                    bidder = bid.get('Bidder', {})
+                    bidder_id = bidder.get('UserID', 'NULL').replace('"', '""')
+                        
+                    with open('bids.dat', 'a') as bids_file:
+                        bids_file.write(f"{item_id}{columnSeparator}\"{bidder_id}\"{columnSeparator}{bid_amount}{columnSeparator}{bid_time}\n")
+        
+def writeToFile(fileName, data):
+    with open(f'{fileName}.dat', 'a') as f:
+        f.write('|'.join(map(str, data)) + '\n')
+def writeToFile(fileName, data):
+    with open(f'{fileName}.dat', 'a') as f:
+        f.write('|'.join(map(str, data)) + '\n')
 
 """
 Loops through each json files provided on the command line and passes each file
@@ -223,7 +126,7 @@ def main(argv):
     for f in argv[1:]:
         if isJson(f):
             parseJson(f)
-            print("Success parsing " + f)
+            print (f"Success parsing {f}")
 
 if __name__ == '__main__':
     main(sys.argv)
