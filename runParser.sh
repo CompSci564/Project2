ba6ceb4f207980fb22280bdca21828e6e94d512c
youngwoo <ykim846@wisc.edu>
2024-02-17 01:22:31 -0600

    seven more sqlite commands on shell scripts

--- runParser.sh
+++ runParser.sh
@@ -23,3 +23,11 @@ mv $temp_file $input_file
 rm *.db
 sqlite3 ebay.db < create.sql
 sqlite3 ebay.db < load.txt
+
+sqlite3 ebay.db < query1.sql
+sqlite3 ebay.db < query2.sql
+sqlite3 ebay.db < query3.sql
+sqlite3 ebay.db < query4.sql
+sqlite3 ebay.db < query5.sql
+sqlite3 ebay.db < query6.sql
+sqlite3 ebay.db < query7.sql
