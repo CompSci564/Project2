fa87a69849a5aa5df486938666e222677e053fc6
youngwoo <ykim846@wisc.edu>
2024-02-18 23:58:29 -0600

    edit runParser.sh to automate sorting for files

--- runParser.sh
+++ runParser.sh
@@ -2,32 +2,50 @@
 rm -f *.dat
 python3 skeleton_parser.py ebay_data/items-*.json
 
-input_file="users.dat"
-temp_file="${input_file}.tmp"
+# input_file="users.dat"
+# temp_file="${input_file}.tmp"
 
-# Use awk to remove duplicate lines based on the first field
-sort -k1,1 -k2,2 $input_file | awk '!seen[$1]++' $input_file > $temp_file
+# # Use awk to remove duplicate lines based on the first field
+# awk -F '|' '!seen[$1]++' $input_file > $temp_file
 
-# Move the temporary file back to the original file
-mv $temp_file $input_file
+# # Move the temporary file back to the original file
+# mv $temp_file $input_file
 
-input_file="categories.dat"
-temp_file="${input_file}.tmp"
+# input_file="categories.dat"
+# temp_file="${input_file}.tmp"
 
-# Use awk to remove duplicate lines based on the first and second fields
-sort -k1,1 -k2,2 $input_file | awk -F'|' '!seen[$1, $2]++' $input_file > $temp_file
+# # Use awk to remove duplicate lines based on the first and second fields
+# awk -F'|' '!seen[$1, $2]++' $input_file > $temp_file
 
-# Move the temporary file back to the original file
-mv $temp_file $input_file
+# # Move the temporary file back to the original file
+# mv $temp_file $input_file
+
+remove_duplicates() {
+  local input_file="$1"
+  local temp_file="${input_file}.tmp"
+
+  # Use awk to remove duplicate lines based on the first and second fields
+  awk -F'|' '!seen[$1, $2]++' "$input_file" > "$temp_file"
+
+  # Move the temporary file back to the original file
+  mv "$temp_file" "$input_file"
+}
+
+# Process each specified file
+for file in "users.dat" "categories.dat";
+do
+  remove_duplicates "$file"
+done
 
 rm *.db
 sqlite3 ebay.db < create.sql
 sqlite3 ebay.db < load.txt
 
-sqlite3 ebay.db < query1.sql
-sqlite3 ebay.db < query2.sql
-sqlite3 ebay.db < query3.sql
-sqlite3 ebay.db < query4.sql
-sqlite3 ebay.db < query5.sql
-sqlite3 ebay.db < query6.sql
-sqlite3 ebay.db < query7.sql
+rm *.dat
+
+for i in {1..7}
+do
+    sqlite3 ebay.db  ".read query$i.sql"
+done
+
+rm *.db
